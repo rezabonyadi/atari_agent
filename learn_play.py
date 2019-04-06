4d8bf2246090270a764ab1a1adede3b144367148
Reza <Rezabny@gmail.com>
2019-04-06 19:57:47 +1000

    comm

--- learn_play.py
+++ learn_play.py
@@ -4,47 +4,20 @@ import numpy as np
 import datetime
 import utils
 
-MAX_EPISODE_LENGTH = 108000       # Equivalent of 5 minutes of gameplay at 60 frames per second
-NO_OP_STEPS = 10                 # Number of 'NOOP' or 'FIRE' actions at the beginning of an
-                                 # evaluation episode
-
-MAX_EPISODES = 1000000
-AGENT_HISTORY_LENGTH = 4
-UPDATE_FREQ = 4                  # Every four actions a gradient descend step is performed
-NETW_UPDATE_FREQ = 10000         # Number of chosen actions between updating the target network.
-                                 # According to Mnih et al. 2015 this is measured in the number of
-                                 # parameter updates (every four actions), however, in the
-                                 # DeepMind code, it is clearly measured in the number
-                                 # of actions the agent choses
-REPLAY_MEMORY_START_SIZE = 50000 # Number of completely random actions,
-                                 # before the agent starts learning
-DISCOUNT_FACTOR = 0.99           # gamma in the Bellman equation
-MEMORY_SIZE = 1000000            # Number of transitions stored in the replay memory
-BS = 32                          # Batch size
-LEARNING_RATE = 0.0001          # Set to 0.00025 in Pong for quicker results.
-                                 # Hessel et al. 2017 used 0.0000625
-PUNISH = 1.0
-INI_EPSILON = 1.0
-END_EPSILON = .1
-MIN_OBSERVE_EPISODE = 200
-
 GAME_ENV = 'BreakoutDeterministic-v4'
 # GAME_ENV = 'SpaceInvaders-v0'
 # GAME_ENV = 'PongDeterministic-v4'
 # GAME_ENV = 'Alien-v4'
 
+
 def main_loop(load_folder=''):
 
     if load_folder is not '':
-        player, game_env, MAX_EPISODE_LENGTH, MAX_EPISODES = utils.load_settings(load_folder)
+        player, game_env, MAX_EPISODE_LENGTH, MAX_EPISODES, all_settings = utils.load_settings(load_folder)
     else:
-        player, game_env, MAX_EPISODE_LENGTH, MAX_EPISODES = utils.load_settings('')
+        player, game_env, MAX_EPISODE_LENGTH, MAX_EPISODES, all_settings = utils.load_default(GAME_ENV)
 
-        # game_env = Atari(GAME_ENV, frame_height, frame_width, agent_history_length=AGENT_HISTORY_LENGTH,
-        #     no_op_steps=NO_OP_STEPS)
-        # player = Player(game_env, AGENT_HISTORY_LENGTH, MEMORY_SIZE, BS,
-        #     LEARNING_RATE, INI_EPSILON, END_EPSILON, MIN_OBSERVE_EPISODE,
-        #     NETW_UPDATE_FREQ, UPDATE_FREQ, DISCOUNT_FACTOR, REPLAY_MEMORY_START_SIZE, PUNISH)
+    utils.save_settings(all_settings, player)
 
     highest_reward = 0
     total_frames = 0.0
@@ -88,4 +61,4 @@ def main_loop(load_folder=''):
                   ' , loss is ', str(np.mean(player.losses[-100:])),
                   ' , fps is ', str(total_frames/time_passed))
 
-main_loop(load_folder='./')
+main_loop()
--- utils.py
+++ utils.py
@@ -8,45 +8,43 @@ import json
 folder_to_use = ''
 settings_file_name = 'settings.jsn'
 
-def save_settings(MAX_EPISODE_LENGTH, NO_OP_STEPS, MAX_EPISODES, AGENT_HISTORY_LENGTH, UPDATE_FREQ, NETW_UPDATE_FREQ, \
-           REPLAY_MEMORY_START_SIZE, DISCOUNT_FACTOR, MEMORY_SIZE, BS, LEARNING_RATE, PUNISH, INI_EPSILON, END_EPSILON, \
-           MIN_OBSERVE_EPISODE, GAME_ENV, player):
+
+def save_settings(settings, player):
+    GAME_ENV = settings['GAME_ENV']
     d = datetime.datetime.now().strftime("%y_%m_%d_%H_%M_%S")
     folder_to_use = ''.join(['./output/', GAME_ENV, '/results_', d, '/'])
     os.makedirs(folder_to_use)
+
+    settings_dict = settings
+
+    with open(''.join([folder_to_use, 'settings.jsn']), 'wt') as outfile:
+        json.dump(settings_dict, outfile)
+
     player.save_player_learner(''.join([folder_to_use, 'main_learner_init.mdl']))
 
-    pass
 
+def load_default(GAME_ENV):
+    settings_dict = {}
+    file_name = './default_settings.jsn'  # default_settings.jsn i in the root
+    with open(file_name, 'rt') as json_file:
+        settings_dict = json.load(json_file)
+    settings_dict['GAME_ENV'] = GAME_ENV
+    game_env = Atari(settings_dict['GAME_ENV'], settings_dict['frame_height'], settings_dict['frame_width'],
+                     agent_history_length=settings_dict['AGENT_HISTORY_LENGTH'],
+                     no_op_steps=settings_dict['NO_OP_STEPS'])
+
+    player = Player(game_env, settings_dict['AGENT_HISTORY_LENGTH'], settings_dict['MEMORY_SIZE'], settings_dict['BS'],
+                    settings_dict['LEARNING_RATE'], settings_dict['INI_EPSILON'], settings_dict['END_EPSILON'],
+                    settings_dict['MIN_OBSERVE_EPISODE'], settings_dict['NETW_UPDATE_FREQ'],
+                    settings_dict['UPDATE_FREQ'],
+                    settings_dict['DISCOUNT_FACTOR'], settings_dict['REPLAY_MEMORY_START_SIZE'],
+                    settings_dict['PUNISH'])
+    return player, game_env, settings_dict['MAX_EPISODE_LENGTH'], settings_dict['MAX_EPISODES'], settings_dict
 
 def load_settings(folder):
     settings_dict = {}
-    if folder is not '':
-        with open(''.join([folder, settings_file_name]), 'rt') as json_file:
-            settings_dict = json.load(json_file)
-    else:
-        settings_dict['MAX_EPISODE_LENGTH'] = 108000  # Equivalent of 5 minutes of gameplay at 60 frames per second
-        settings_dict['NO_OP_STEPS'] = 10  # Number of 'NOOP' or 'FIRE' actions at the beginning of an evaluation episode
-        settings_dict['MAX_EPISODES'] = 1000000
-        settings_dict['AGENT_HISTORY_LENGTH'] = 4
-        settings_dict['UPDATE_FREQ'] = 4  # Every four actions a gradient descend step is performed
-        settings_dict['NETW_UPDATE_FREQ'] = 10000  # Number of chosen actions between updating the target network.
-        settings_dict['REPLAY_MEMORY_START_SIZE'] = 50000  # Number of completely random actions, before the agent starts learning
-        settings_dict['DISCOUNT_FACTOR'] = 0.99  # gamma in the Bellman equation
-        settings_dict['MEMORY_SIZE'] = 1000000  # Number of transitions stored in the replay memory
-        settings_dict['BS'] = 32  # Batch size
-        settings_dict['LEARNING_RATE'] = 0.0001  # Set to 0.00025 in Pong for quicker results.
-        settings_dict['PUNISH'] = 1.0
-        settings_dict['INI_EPSILON'] = 1.0
-        settings_dict['END_EPSILON'] = .1
-        settings_dict['MIN_OBSERVE_EPISODE'] = 200
-
-        settings_dict['GAME_ENV'] = 'BreakoutDeterministic-v4'
-        settings_dict['frame_height'] = 84
-        settings_dict['frame_width'] = 84
-
-        with open('default_settings.jsn', 'wt') as outfile:
-            json.dump(settings_dict, outfile)
+    with open(''.join([folder, settings_file_name]), 'rt') as json_file:
+        settings_dict = json.load(json_file)
 
     game_env = Atari(settings_dict['GAME_ENV'], settings_dict['frame_height'], settings_dict['frame_width'],
                      agent_history_length=settings_dict['AGENT_HISTORY_LENGTH'], no_op_steps=settings_dict['NO_OP_STEPS'])
@@ -56,10 +54,9 @@ def load_settings(folder):
                     settings_dict['MIN_OBSERVE_EPISODE'], settings_dict['NETW_UPDATE_FREQ'], settings_dict['UPDATE_FREQ'],
                     settings_dict['DISCOUNT_FACTOR'], settings_dict['REPLAY_MEMORY_START_SIZE'], settings_dict['PUNISH'])
 
-    if folder is not '':
-        player.load_player_learner(''.join([folder, 'main_learner.mdl']))
+    player.load_player_learner(''.join([folder, 'main_learner.mdl']))
 
-    return player, game_env, settings_dict.MAX_EPISODE_LENGTH, settings_dict.MAX_EPISODES
+    return player, game_env, settings_dict['MAX_EPISODE_LENGTH'], settings_dict['MAX_EPISODES'], settings_dict
 
 def save_res():
     pass
