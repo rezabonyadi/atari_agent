cc915186532e62a666142fe5a87d43554b9b1a7d
rezabonyadi <Reza@1983>
2019-04-26 15:30:33 +1000

    cmm

--- learn_play.py
+++ learn_play.py
@@ -135,9 +135,10 @@ def learn_by_game(results_handler, load_folder='', load_model=False):
 # OUT_FOLDER = './output/Punish_0_No_Reward_exploration/'
 # OUT_FOLDER = './output/Punish_1_No_Reward_exploration/'
 # OUT_FOLDER = './output/Punish_1_Reward_exploration_linear/'
-OUT_FOLDER = './output/original_te/'
+OUT_FOLDER = './output/punish/'
 
-games = ['BreakoutDeterministic-v4', 'QbertDeterministic-v4', 'BerzerkDeterministic-v4']
+games = ['BreakoutDeterministic-v4', 'AsterixDeterministic-v4', 'CarnivalDeterministic-v4', 'MsPacmanDeterministic-v4',
+    'UpNDownDeterministic-v4', 'AssaultDeterministic-v4']
 
 for GAME_ENV in games:
     handler = HandleResults(GAME_ENV, OUT_FOLDER)
