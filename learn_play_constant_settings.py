df59d11f9b49c575fa98e930e2457f63b46898e1
rezabonyadi <Reza@1983>
2019-04-28 08:00:00 +1000

    Setting file used again, a separate python added for constant

--- learn_play_constant_settings.py
+++ learn_play_constant_settings.py
@@ -138,10 +138,11 @@ def learn_by_game(results_handler, load_folder='', load_model=False):
 # OUT_FOLDER = './output/Punish_0_No_Reward_exploration/'
 # OUT_FOLDER = './output/Punish_1_No_Reward_exploration/'
 # OUT_FOLDER = './output/Punish_1_Reward_exploration_linear/'
-OUT_FOLDER = './output/punish_10/'
+OUT_FOLDER = './output/original/'
 
 games = ['BreakoutDeterministic-v4', 'AsterixDeterministic-v4', 'CarnivalDeterministic-v4', 'MsPacmanDeterministic-v4',
-    'UpNDownDeterministic-v4', 'AssaultDeterministic-v4']
+    'UpNDownDeterministic-v4', 'AssaultDeterministic-v4', 'BerzerkDeterministic-v4',
+         'QbertDeterministic-v4', 'AmidarDeterministic-v4', 'SpaceInvadersDeterministic-v4']
 
 for GAME_ENV in games:
     handler = HandleResults(GAME_ENV, OUT_FOLDER)
