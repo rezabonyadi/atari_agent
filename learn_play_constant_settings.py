f9f3d1c6c05414eb33629894b26d711e2aa82594
rezabonyadi <Reza@1983>
2019-05-02 22:31:21 +1000

    vs

--- learn_play_constant_settings.py
+++ learn_play_constant_settings.py
@@ -145,7 +145,7 @@ def learn_by_game(results_handler, load_folder='', load_model=False):
 # OUT_FOLDER = './output/Punish_0_No_Reward_exploration/'
 # OUT_FOLDER = './output/Punish_1_No_Reward_exploration/'
 # OUT_FOLDER = './output/Punish_1_Reward_exploration_linear/'
-OUT_FOLDER = './output/punish_1/'
+OUT_FOLDER = './output/punish_10/'
 
 # games = [
 #     'BreakoutDeterministic-v4', 'AsterixDeterministic-v4', 'CarnivalDeterministic-v4', 'MsPacmanDeterministic-v4',
