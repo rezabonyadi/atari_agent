d70b8fb6dea212a514f289f1f06b763e1e71c37b
Reza <Rezabny@gmail.com>
2019-05-03 14:06:03 +1000

    Run command line

--- g_tes_j.sh
+++ g_tes_j.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=p200
+#SBATCH --job-name=p100
 #SBATCH -n 2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32000
-#SBATCH -o re_out_p200.txt
-#SBATCH -e re_error_p200.txt
+#SBATCH -o re_out_p100.txt
+#SBATCH -e re_error_p100.txt
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
