59571094231fd7ab0ed2b2da8d17f3e79b07c858
rezabonyadi <Reza@1983>
2019-05-04 20:17:35 +1000

    vsg

--- g_tes_r.sh
+++ g_tes_r.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=p100_Tuk
+#SBATCH --job-name=p100_Riv
 #SBATCH -n 2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32000
-#SBATCH -o re_out_p100_Tuk.txt
-#SBATCH -e re_error_p100_Tuk.txt
+#SBATCH -o re_out_p100_Riv.txt
+#SBATCH -e re_error_p100_Riv.txt
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
 module load anaconda/3.6
 source activate /opt/ohpc/pub/apps/atari
 
-srun -n2 python ./learn_play_runtime.py TutankhamDeterministic-v4 ./output/punish_100/ 100.0 -2.0 False True --num_gpus=1
+srun -n2 python ./learn_play_runtime.py RiverraidDeterministic-v4 ./output/punish_100/ 100.0 -2.0 False True --num_gpus=1
