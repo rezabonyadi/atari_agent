b94106598913831232aeb139eba657aca24bbe0a
rezabonyadi <Reza@1983>
2019-05-04 20:51:16 +1000

    vsg

--- g_tes_j.sh
+++ g_tes_j.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 module load anaconda/3.6
 source activate /opt/ohpc/pub/apps/atari
 
@@ -12,11 +13,5 @@ source activate /opt/ohpc/pub/apps/atari
 #SBATCH --job-name=p100_Gr1
 #SBATCH -o re_out_p100_Gr1.txt
 #SBATCH -e re_error_p100_Gr1.txt
-
-srun -n2 python ./learn_play_runtime.py GravitarDeterministic-v4 ./output/punish_100_1/ 100.0 -2.0 False True --num_gpus=1 &
-
-#SBATCH --job-name=p100_Gr2
-#SBATCH -o re_out_p100_Gr2.txt
-#SBATCH -e re_error_p100_Gr2.txt
 
 srun -n2 python ./learn_play_runtime.py GravitarDeterministic-v4 ./output/punish_100_1/ 100.0 -2.0 False True --num_gpus=1
