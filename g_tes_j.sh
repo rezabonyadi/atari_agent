76713be77e4c324ea05d8f51bb692417a533e547
rezabonyadi <Reza@1983>
2019-05-04 20:47:13 +1000

    vsg

--- g_tes_j.sh
+++ g_tes_j.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --job-name=p100
-#SBATCH -n 2
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=32000
-#SBATCH -o re_out_p100.txt
-#SBATCH -e re_error_p100.txt
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
-
 module load anaconda/3.6
 source activate /opt/ohpc/pub/apps/atari
 
-srun -n2 python ./learn_play_constant_settings.py --num_gpus=1
+#SBATCH --nodes=1
+#SBATCH -n 2
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=32000
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+#SBATCH --job-name=p100_Grv
+#SBATCH -o re_out_p100_Grv.txt
+#SBATCH -e re_error_p100_Grv.txt
+
+srun -n2 python ./learn_play_runtime.py GravitarDeterministic-v4 ./output/punish_100_1/ 100.0 -2.0 False True --num_gpus=1 &
+
+#SBATCH --job-name=p100_Grv1
+#SBATCH -o re_out_p100_Grv1.txt
+#SBATCH -e re_error_p100_Grv1.txt
+
+srun -n2 python ./learn_play_runtime.py GravitarDeterministic-v4 ./output/punish_100_1/ 100.0 -2.0 False True --num_gpus=1
--- g_tes_r.sh
+++ g_tes_r.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=p100_Grv1
+#SBATCH --job-name=p100_Grv
 #SBATCH -n 2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32000
-#SBATCH -o re_out_p100_Grv1.txt
-#SBATCH -e re_error_p100_Grv1.txt
+#SBATCH -o re_out_p100_Grv.txt
+#SBATCH -e re_error_p100_Grv.txt
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
 module load anaconda/3.6
 source activate /opt/ohpc/pub/apps/atari
 
-srun -n2 python ./learn_play_runtime.py GravitarDeterministic-v4 ./output/punish_100_1/ 10.0 -2.0 False True --num_gpus=1
-
-#SBATCH --nodes=1
-#SBATCH --job-name=p100_Grv2
-#SBATCH -n 2
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=32000
-#SBATCH -o re_out_p100_Grv2.txt
-#SBATCH -e re_error_p100_Grv2.txt
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
-
-module load anaconda/3.6
-source activate /opt/ohpc/pub/apps/atari
-
-srun -n2 python ./learn_play_runtime.py GravitarDeterministic-v4 ./output/punish_100_1/ 100.0 -2.0 False True --num_gpus=1
+srun -n2 python ./learn_play_runtime.py GravitarDeterministic-v4 ./output/punish_100/ 100.0 -2.0 False True --num_gpus=1
