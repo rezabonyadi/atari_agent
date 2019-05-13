9a3c631f7ad4b565d65d27b2a7ca4eb139c87fef
rezabonyadi <Reza@1983>
2019-05-13 19:34:02 +1000

    vsg

--- batch_files/Run_Orig.sh
+++
@@ -1,4 +0,0 @@
-sbatch Orig_bash//BerzerkDeterministic-v4.sh
-sbatch Orig_bash//QbertDeterministic-v4.sh 
-sbatch Orig_bash//AmidarDeterministic-v4.sh 
-sbatch Orig_bash//SpaceInvadersDeterministic-v4.sh 
--- batch_files/Run_e50_p1.sh
+++
@@ -1,1 +0,0 @@
-sbatch e50_p1_bash//GravitarDeterministic-v4.sh
