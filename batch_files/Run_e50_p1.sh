8da7c2771a60908cf3733cdd991e9155e7f2579d
rezabonyadi <vardiar@gmail.com>
2019-12-11 17:38:25 +1000

    run DQN for hard games

--- batch_files/Run_e50_p1.sh
+++ batch_files/Run_e50_p1.sh
@@ -1,1 +1,4 @@
-sbatch e50_p1_bash//PrivateEyeDeterministic-v4.sh 
+sbatch e50_p1_bash//RoadRunnerDeterministic-v4.sh 
+sbatch e50_p1_bash//TutankhamDeterministic-v4.sh 
+sbatch e50_p1_bash//KungFuMasterDeterministic-v4.sh 
+sbatch e50_p1_bash//KrullDeterministic-v4.sh 
--- batch_files/Run_original.sh
+++ batch_files/Run_original.sh
@@ -1,1 +1,4 @@
-sbatch original_bash//AlienDeterministic-v4.sh 
+sbatch original_bash//RoadRunnerDeterministic-v4.sh 
+sbatch original_bash//TutankhamDeterministic-v4.sh 
+sbatch original_bash//KungFuMasterDeterministic-v4.sh 
+sbatch original_bash//KrullDeterministic-v4.sh 
