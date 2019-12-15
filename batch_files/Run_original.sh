4b8b050a5ebfb892d3949493a0a8dc2c2c064121
rezabonyadi <vardiar@gmail.com>
2019-12-15 12:48:58 +1000

    run DQN for hard games

---
+++ batch_files/Run_original.sh
@@ -0,0 +1,4 @@
+sbatch original_bash//RoadRunnerDeterministic-v4.sh 
+sbatch original_bash//TutankhamDeterministic-v4.sh 
+sbatch original_bash//KungFuMasterDeterministic-v4.sh 
+sbatch original_bash//KrullDeterministic-v4.sh 
